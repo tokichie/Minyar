026d7d7dfd15f77c9f07a51d637751c2333dff35
Yuta Tokitake <toki211@gmail.com>
2015-04-28 16:07:27 +0900

    add Octokit client / add some resources

---
+++ Minyar/Github/OctokitClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Octokit;
+using System.IO;
+
+namespace Minyar.Github {
+	public class OctokitClient {
+		private GitHubClient github;
+
+		public OctokitClient() {
+			github = new GitHubClient(new ProductHeaderValue("Minyar"));
+			github.Credentials = new Credentials(IncludeToken());
+		}
+
+		private string IncludeToken() {
+			var token = "";
+			try {
+				var path = Path.Combine("..", "..", "Resources", "AuthInfo.txt");
+				var reader = new StreamReader(path);
+				token = reader.ReadLine().Trim();
+			} catch (IOException e) {
+				Console.WriteLine(e.StackTrace);
+			}
+			return token;
+		}
+	}
+}
+
