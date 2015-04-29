664a0b4eef0a43308cb746dd24606dd1a81fadd5
tokichie <toki211@gmail.com>
2015-04-29 10:26:14 +0900

    change assembly info

--- Minyar.Tests/Properties/AssemblyInfo.cs
+++ Minyar.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XmlGenerator.Tests")]
+[assembly: AssemblyTitle("Minyar.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XmlGenerator.Tests")]
+[assembly: AssemblyProduct("Minyar.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
