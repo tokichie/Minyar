33d538915f98b807fdaab176e174181118865e70
tokichie <toki211@gmail.com>
2015-09-29 15:21:20 +0900

    add custom Item, Itemset

---
+++ library/fp/FP/DAL/DAO/JsonItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace FP.DAO {
+	public class JsonItem {
+		public int SupportCount;
+        [DataMember(Name = "Symbol")]
+	    public string Symbol;
+        [DataMember(Name = "Opration")]
+        public string Operation;
+        [DataMember(Name = "NodeType")]
+        public string NodeType;
+        [DataMember(Name = "OriginalToken")]
+        public string OriginalToken;
+        [DataMember(Name = "ChangedToken")]
+        public string ChangedToken;
+        [DataMember(Name = "OriginalPosition")]
+        public string OriginalPosition;
+        [DataMember(Name = "ChangedPosition")]
+        public string ChangedPosition;
+
+		public JsonItem() {
+		    SupportCount = -1;
+		}
+
+	    public JsonItem Deserialize(string json) {
+	        var serializer = new JavaScriptSerializer();
+	        return serializer.Deserialize<JsonItem>(json);
+	    }
+
+	    public JsonItem Clone() {
+	        JsonItem item = new JsonItem();
+	        item.SupportCount = SupportCount;
+	        item.Symbol = Symbol;
+	        item.Operation = Operation;
+	        item.NodeType = NodeType;
+	        item.OriginalToken = OriginalToken;
+	        item.ChangedToken = ChangedToken;
+	        item.OriginalPosition = OriginalPosition;
+	        item.ChangedPosition = ChangedPosition;
+	        return item;
+	    }
+
+	    public override string ToString() {
+	        var sb = new StringBuilder();
+	        sb.Append("<").Append(Operation).Append(":").Append(NodeType);
+	        var tokenChange = new StringBuilder();
+	        var positionChange = new StringBuilder();
+	        if (Operation == "Insert") {
+	            tokenChange.Append(" ").Append(ChangedToken);
+                positionChange.Append(" ").Append(ChangedPosition);
+	        } else if (Operation == "Update") {
+	            tokenChange.Append(" ").Append(OriginalToken).Append(" -> ").Append(ChangedToken);
+                positionChange.Append(" ").Append(OriginalPosition).Append(" -> ").Append(ChangedPosition);
+            } else if (Operation == "Move") {
+                tokenChange.Append(" ").Append(OriginalToken);
+                positionChange.Append(" ").Append(OriginalPosition).Append(" -> ").Append(ChangedPosition);
+            } else if (Operation == "Delete") {
+                tokenChange.Append(" ").Append(OriginalToken);
+                positionChange.Append(" ").Append(ChangedPosition);
+            }
+            sb.Append(tokenChange.ToString().Trim())
+                .Append(positionChange.ToString().Trim()).Append(">");
+	        return sb.ToString();
+	    }
+	}
+}
+
---
+++ library/fp/FP/DAL/DAO/JsonItemSet.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FP.DAO;
+
+namespace FP.DAL.DAO {
+    class JsonItemSet {
+        private List<JsonItem> items;
+
+        public int SupportCount { get; private set; }
+        public string OriginalPath { get; private set; }
+        public string ChangedPath { get; private set; }
+
+        public JsonItemSet(string originalPath, string changedPath) {
+            items = new List<JsonItem>();
+            SupportCount = -1;
+            OriginalPath = originalPath;
+            ChangedPath = changedPath;
+        }
+
+        public void AddItem(JsonItem item) {
+            items.Add(item);
+            SupportCount = -1;
+        }
+        //remove item
+        public JsonItem GetItem(int position) {
+            if (position < items.Count)
+                return items[position];
+            else
+                return null;
+        }
+        //add item into item set
+        public bool IsEmpty() {
+            return items.Count == 0;
+        }
+        //add item into item set
+        public int GetLength() {
+            return items.Count;
+        }
+
+        public JsonItemSet Clone() {
+            JsonItemSet itemSet = new JsonItemSet(OriginalPath, ChangedPath);
+            itemSet.SupportCount = SupportCount;
+            foreach (JsonItem anItem in items) {
+                itemSet.AddItem(anItem.Clone());
+            }
+            return itemSet;
+        }
+
+        public string GetInfoString() {
+            string info = "";
+
+            foreach (JsonItem anItem in items) {
+                info += (" " + anItem.Symbol);
+            }
+
+            return info;
+        }
+
+        public void Print() {
+            Console.WriteLine(SupportCount);
+            foreach (JsonItem item in items) {
+                Console.Write(item.Symbol + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public JsonItem GetLastItem() {
+            return items.Last();
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            if (items.Count > 0) {
+                foreach (var item in items) {
+                    sb.Append(item).Append(", ");
+                }
+                sb.Remove(sb.Length - 2, 2);
+            }
+            if (OriginalPath != ChangedPath) {
+                return string.Format("[SupportCount={0} Path:{1} -> {2} {3}]", SupportCount, OriginalPath, ChangedPath, sb);
+            }
+            return string.Format("[SupportCount={0} Path:{1} {2} ]", SupportCount, OriginalPath, sb);
+        }
+    }
+}
