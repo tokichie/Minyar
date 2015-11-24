30d91e28f777cddf56046ebd7e1d4fd2da2b3951
tokichie <toki211@gmail.com>
2015-11-25 00:40:07 +0900

    Create DiffPatcher

---
+++ Minyar/Github/DiffPatcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace Minyar.Github {
+    class DiffPatcher {
+        public DiffPatcher(PullRequestReviewComment prComment) {
+            
+        }
+    }
+}
