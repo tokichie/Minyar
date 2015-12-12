848c0fb0a4a6569d70ab7a721c9af96358a91f6b
tokichie <toki211@gmail.com>
2015-12-12 20:05:18 +0900

    Create project MinyarClinet

---
+++ MinyarClient/Model/MainModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace MinyarClient.Model {
+    public class MainModel {
+        public string FilePath { get; private set; }
+        public bool FileSelected { get; private set; }
+        public string Threshold { get; set; }
+
+        public MainModel() {
+            Threshold = "200";
+        }
+
+        public void SelectFile() {
+            var dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "Text File|*.txt|All Files|*.*";
+            var res = dialog.ShowDialog();
+            if (res == true) {
+                FilePath = dialog.FileName;
+                FileSelected = true;
+            }
+        }
+    }
+}
---
+++ MinyarClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using MinyarClient.Model;
+
+namespace MinyarClient.ViewModel {
+    public class MainViewModel : ViewModelBase {
+        public MainModel MyMainModel { get; set; }
+
+        public MainViewModel() {
+            MyMainModel = new MainModel();
+        }
+
+        public void SelectFile() {
+            MyMainModel.SelectFile();
+            RaisePropertyChanged("MyMainModel");
+        }
+
+        public void StartMining() {
+            
+            RaisePropertyChanged("MyMainModel");
+        }
+    }
+}
