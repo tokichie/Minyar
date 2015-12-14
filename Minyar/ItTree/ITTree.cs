9b37163e9372973626c790946bcf416d0ed6759c
tokichie <toki211@gmail.com>
2015-12-15 00:16:16 +0900

    Add some classes

---
+++ Minyar/ItTree/ITTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code2Xml.Core.SyntaxTree;
+using FP.DAL.DAO;
+using Paraiba.Linq;
+
+namespace Minyar.ItTree {
+    public class ItTree {
+        private HashSet<ItemTidset> transactions;
+        private int minSup;
+
+        public ItTree(HashSet<ItemTidset> transactions, int minSup) {
+            this.transactions = transactions;
+            this.minSup = minSup;
+        }
+
+        public List<SortedSet<string>> GetClosedItemSets() {
+            return Charm();
+        }
+
+        private List<SortedSet<string>> Charm() {
+            var res = new List<SortedSet<string>>();
+            CharmExtend(transactions, res);
+            return res;
+        }
+
+        private void CharmExtend(HashSet<ItemTidset> nodes, List<SortedSet<string>> closedSets) {
+            foreach (var nodeI in nodes) {
+                var itemSets = new SortedSet<string>(nodeI.TransactionItems);
+                var newNodes = new HashSet<ItemTidset>();
+                foreach (var nodeJ in nodes.Where(n => n >= nodeI)) {
+                    var transactionSets = new HashSet<int>(nodeI.TransactionIds);
+                    itemSets.UnionWith(nodeJ.TransactionItems);
+                    transactionSets.IntersectWith(nodeJ.TransactionIds);
+                    CharmProperty(nodes, newNodes, nodeI, nodeJ, itemSets, transactionSets);
+                }
+                if (!newNodes.IsEmpty()) CharmExtend(newNodes, closedSets);
+            }
+        }
+
+        private void CharmProperty(HashSet<ItemTidset> nodes, HashSet<ItemTidset> newNodes, ItemTidset nodeI, ItemTidset nodeJ, SortedSet<string> itemSets, HashSet<int> transactionSets) {
+            if (transactionSets.Count < minSup) return;
+            var xi = nodeI.TransactionIds;
+            var xj = nodeJ.TransactionIds;
+            if (xi.SetEquals(xj)) {
+                nodes.Remove(nodeJ);
+            } else if (xi.IsSubsetOf(xj)) {
+                
+            } else if (xi.IsProperSupersetOf(xj)) {
+                nodes.Remove(nodeJ);
+                newNodes.Add(new ItemTidset(itemSets, transactionSets));
+            } else if (!xi.Overlaps(xj)) {
+                newNodes.Add(new ItemTidset(itemSets, transactionSets));
+            }
+        }
+    }
+}
---
+++ Minyar/ItTree/ItemTidset.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FP.DAL.DAO;
+
+namespace Minyar.ItTree {
+    public class ItemTidset : IComparable {
+        public SortedSet<string> TransactionItems;
+        public HashSet<int> TransactionIds;
+
+        public ItemTidset(SortedSet<string> items, HashSet<int> ids) {
+            TransactionItems = items;
+            TransactionIds = ids;
+        }
+
+        public static bool operator >=(ItemTidset left, ItemTidset right) {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public static bool operator <=(ItemTidset left, ItemTidset right) {
+            return left.CompareTo(right) < 0;
+        }
+
+        public int CompareTo(object obj) {
+            if (obj == null) return 1;
+            var right = (ItemTidset) obj;
+            return ToString().CompareTo(right.ToString());
+        }
+
+        public override string ToString() {
+            var sb = new StringBuilder();
+            foreach (var item in TransactionItems) {
+                sb.Append(item);
+            }
+            return sb.ToString();
+        }
+    }
+}
