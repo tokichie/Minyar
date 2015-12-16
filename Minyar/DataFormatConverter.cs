ee867f672a931fc8873bd7701453eaa493a2f6a4
tokichie <toki211@gmail.com>
2015-12-16 19:08:25 +0900

    wip

---
+++ Minyar/DataFormatConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FP.DAL.DAO;
+using Minyar.Charm;
+
+namespace Minyar {
+    public class DataFormatConverter {
+        public static List<ItemTidSet<string, RepeatableTid>> HorizontalToVertical(List<ItemWrapper> data) {
+            var res = new List<ItemTidSet<string, RepeatableTid>>();
+            int tid = 0;
+            foreach (var item in data) {
+                tid++;
+                var counter = new Dictionary<int, Dictionary<string, int>>(); 
+                foreach (var symbol in item.Items.Select(i => i.Symbol)) {
+                    counter[tid] = new Dictionary<string, int>();
+                    if (!counter[tid].ContainsKey(symbol)) {
+                        counter[tid][symbol] = 0;
+                    } else {
+                        counter[tid][symbol]++;
+                    }
+                }
+                res.Add(new ItemTidSet<string, RepeatableTid>(new [] {item.}));
+            }
+        }
+    }
+}
---
+++ Minyar/ItTreeMiner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FP.DAL.DAO;
+using Minyar.Charm;
+using Paraiba.IO;
+
+namespace Minyar {
+    public class ItTreeMiner {
+        private ItTree itTree;
+        private string inputFilePath;
+        public int Threshold;
+
+        public ItTreeMiner(string inputPath, int threshold = -1) {
+            Threshold = threshold;
+            inputFilePath = inputPath;
+        }
+
+        public void GenerateClosedItemsets() {
+            int lineCount = 0;
+            using (var reader = new StreamReader(inputFilePath)) {
+                lineCount++;
+                var data = new List<ItemWrapper>();
+                foreach (var line in reader.ReadLines()) {
+                    var itemWrapper = ItemWrapper.Deserialize(line.Trim());
+                    data.Add(itemWrapper);
+                }
+            }
+            if (Threshold == -1) Threshold = lineCount / 10 * 2;
+        }
+    }
+}
